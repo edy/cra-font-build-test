56a74c4946ed8f07ed231797f21d9fe68a675177
Eduard Baun <e.baun@loom-technologies.com>
2018-06-26 12:02:18 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Load Aguafina Script typeface
+import 'typeface-aguafina-script';
+import './index.css';
+
+import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
